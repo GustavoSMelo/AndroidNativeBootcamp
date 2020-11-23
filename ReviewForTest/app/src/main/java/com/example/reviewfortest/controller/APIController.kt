a6e4b55d19f14f115b349d99dea55c983ddfc89c
Gustavo S. Melo <gsantos15569@gmail.com>
2020-11-23 00:47:34 -0300

    code of api

--- ReviewForTest/app/src/main/java/com/example/reviewfortest/controller/APIController.kt
+++ ReviewForTest/app/src/main/java/com/example/reviewfortest/controller/APIController.kt
@@ -1,4 +1,10 @@
 package com.example.reviewfortest.controller
 
+import java.net.URL
+
 class APIController {
+    fun getDataByAPI () : String {
+        val response = URL("http://mfpledon.com.br/receita.json").readText()
+        return response
+    }
 }
